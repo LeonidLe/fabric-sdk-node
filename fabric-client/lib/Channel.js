d8f3bf6e80c6a3f69142b82d1981f8d99c26eb93
LeonidLe <21340602+LeonidLe@users.noreply.github.com>
2019-02-05 19:50:12 +0300

    apply connection options to peers created at channel discovery

--- fabric-client/lib/Channel.js
+++ fabric-client/lib/Channel.js
@@ -1502,9 +1502,10 @@ const Channel = class {
 			'ssl-target-name-override': host,
 			'name': name
 		};
-		this._clientContext.addTlsClientCertAndKey(opts);
+		const connectionOptions = this._clientContext._buildConnectionOptions(opts);
+		this._clientContext.addTlsClientCertAndKey(connectionOptions);
 
-		return opts;
+		return connectionOptions;
 	}
 
 	_buildTlsRootCerts(msp) {
